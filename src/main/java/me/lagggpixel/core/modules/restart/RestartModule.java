d6af84d40470e4b0d57d42460cb443171c9f3a6e
Lagggpixel <lagggpixel@gmail.com>
2023-09-16 23:38:52 +0100

    feat: Added restart module

---
+++ src/main/java/me/lagggpixel/core/modules/restart/RestartModule.java
@@ -0,0 +1,27 @@
+package me.lagggpixel.core.modules.restart;
+
+import me.lagggpixel.core.modules.Module;
+import org.jetbrains.annotations.NotNull;
+
+public class RestartModule extends Module {
+    @NotNull
+    @Override
+    public String getId() {
+        return "restart";
+    }
+
+    @Override
+    public void initialize() {
+
+    }
+
+    @Override
+    public void registerCommands() {
+
+    }
+
+    @Override
+    public void registerListeners() {
+
+    }
+}
