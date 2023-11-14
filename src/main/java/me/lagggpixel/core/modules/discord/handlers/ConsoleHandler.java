103f516941d3233a8e2b40f39b7e03b33e147c8d
Lagggpixel <lagggpixel@gmail.com>
2023-11-14 10:25:31 +0000

    fix(discord): fixed console handling with discord rate limits
    Took 28 minutes

--- src/main/java/me/lagggpixel/core/modules/discord/handlers/ConsoleHandler.java
+++ src/main/java/me/lagggpixel/core/modules/discord/handlers/ConsoleHandler.java
@@ -1,8 +1,10 @@
 package me.lagggpixel.core.modules.discord.handlers;
 
 import me.lagggpixel.core.modules.discord.managers.DiscordManager;
-import net.dv8tion.jda.api.utils.messages.MessageCreateBuilder;
+import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.utils.messages.*;
 import org.apache.logging.log4j.core.LogEvent;
+import org.bukkit.util.NumberConversions;
 
 import java.time.Instant;
 import java.time.LocalDateTime;
@@ -12,12 +14,28 @@ import java.util.TimeZone;
 
 public class ConsoleHandler {
   private static final DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+  private static Integer time;
+  private static Message message;
+  private static MessageData messageData;
 
   public static void onLogEvent(LogEvent logEvent) {
-    MessageCreateBuilder builder = new MessageCreateBuilder();
-    builder.setContent(formatLoggingMessage(logEvent));
-    builder.setEmbeds(Collections.emptyList());
-    DiscordManager.getInstance().CONSOLE_CHANNEL.sendMessage(builder.build()).queue();
+    Integer currentTime = NumberConversions.floor((double) LocalDateTime.now().getMinute() / 5) * 5;
+    if (message == null || !currentTime.equals(time)) {
+      time = NumberConversions.floor((double) LocalDateTime.now().getMinute() / 5) * 5;
+      MessageCreateBuilder builder = new MessageCreateBuilder();
+      builder.setContent(formatLoggingMessage(logEvent));
+      builder.setEmbeds(Collections.emptyList());
+      messageData = builder.build();
+      DiscordManager.getInstance().CONSOLE_CHANNEL.sendMessage(builder.build()).queue((message) -> ConsoleHandler.message = message);
+      return;
+    }
+    
+    MessageEditBuilder builder = new MessageEditBuilder();
+    builder.setContent(messageData.getContent() + "\n" + formatLoggingMessage(logEvent));
+    messageData = builder.build();
+    message.editMessage(builder.build()).queue((message) -> {
+      ConsoleHandler.message = message;
+    });
   }
 
   private static String stripAnsiColors(String input) {
