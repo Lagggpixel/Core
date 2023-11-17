459f055f268b87a911f17c68a215cb370a3d1255
Lagggpixel <lagggpixel@gmail.com>
2023-11-17 18:32:07 +0000

    Revert "feat(staff): Added armour stands to staff mode"
    This reverts commit d5cc12e9cb9088df2b946df73d99b6316591e4e1.
    
    
    Took 1 hour 36 minutes

--- src/main/java/me/lagggpixel/core/modules/staff/handlers/StaffModeHandler.java
+++ src/main/java/me/lagggpixel/core/modules/staff/handlers/StaffModeHandler.java
@@ -3,27 +3,13 @@ package me.lagggpixel.core.modules.staff.handlers;
 import me.lagggpixel.core.Main;
 import me.lagggpixel.core.data.User;
 import me.lagggpixel.core.modules.staff.data.InstantPlayerData;
-import me.lagggpixel.core.utils.ChatUtils;
-import net.kyori.adventure.text.format.TextColor;
-import org.bukkit.Color;
 import org.bukkit.GameMode;
-import org.bukkit.entity.ArmorStand;
 import org.bukkit.entity.Player;
-
-import java.util.HashMap;
-import java.util.UUID;
 
 public class StaffModeHandler {
   
-  private final HashMap<UUID, ArmorStand> staffModeArmorStands = new HashMap<>();
-  
   public void enterStaffMode(Player player) {
     User user = Main.getUser(player.getUniqueId());
-    
-    if (user.isStaffMode()) {
-      return;
-    }
-    
     user.setStaffMode(true);
     InstantPlayerData instantPlayerData = new InstantPlayerData(player);
     user.setInstantPlayerData(instantPlayerData);
@@ -33,30 +19,10 @@ public class StaffModeHandler {
     player.setFoodLevel(20);
     player.setFlying(true);
     player.setAffectsSpawning(false);
-    
-    ArmorStand as = player.getLocation().getWorld().spawn(player.getLocation().add(0, 2.5, 0), ArmorStand.class);
-    staffModeArmorStands.put(player.getUniqueId(), as);
-    
-    as.setGravity(false);
-    as.setCanPickupItems(false);
-    as.customName(ChatUtils.stringToComponent("Staff Mode").color(TextColor.color(Color.GRAY.asRGB())));
-    as.setCustomNameVisible(true);
-    as.setVisible(false);
-    player.addPassenger(as);
   }
   
   public void exitStaffMode(Player player) {
     User user = Main.getUser(player.getUniqueId());
-    
-    if (!user.isStaffMode()) {
-      return;
-    }
-    
-    ArmorStand as = staffModeArmorStands.get(player.getUniqueId());
-    player.removePassenger(as);
-    as.remove();
-    staffModeArmorStands.remove(player.getUniqueId());
-    
     user.setStaffMode(false);
     InstantPlayerData instantPlayerData = user.getInstantPlayerData();
     instantPlayerData.restorePlayerData(player);
