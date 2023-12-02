2d579f464f11ad95f6ae7eb1bef7b2f3e029eb40
Wanbin Cao <93872019+Lagggpixel@users.noreply.github.com>
2023-12-02 15:21:29 +0000

    feat(guild): added guild invite
    Took 13 seconds

--- src/main/java/me/lagggpixel/core/modules/guilds/commands/subCommands/GuildInviteCommand.java
+++ src/main/java/me/lagggpixel/core/modules/guilds/commands/subCommands/GuildInviteCommand.java
@@ -1,18 +1,84 @@
 package me.lagggpixel.core.modules.guilds.commands.subCommands;
 
+import me.lagggpixel.core.data.Lang;
 import me.lagggpixel.core.modules.guilds.GuildModule;
 import me.lagggpixel.core.modules.guilds.commands.ISubCommand;
+import me.lagggpixel.core.modules.guilds.data.Guild;
+import net.kyori.adventure.text.TextComponent;
+import net.kyori.adventure.text.event.ClickEvent;
+import net.kyori.adventure.text.event.HoverEvent;
+import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import java.util.Map;
 
 public class GuildInviteCommand implements ISubCommand {
   private final GuildModule guildModule;
-  
+
   public GuildInviteCommand(GuildModule guildModule) {
     this.guildModule = guildModule;
   }
-  
+
   @Override
-  public void execute(CommandSender sender, String[] args) {
-    // TODO: Implement guild invite logic here
+  public void execute(CommandSender commandSender, String[] args) {
+    if (!(commandSender instanceof Player sender)) {
+      commandSender.sendMessage(Lang.PLAYER_ONLY.toComponentWithPrefix());
+      return;
+    }
+
+    if (args.length == 0) {
+      sender.sendMessage(Lang.INVALID_USAGE.toComponentWithPrefix());
+      return;
+    }
+
+    if (guildModule.getGuildHandler().getGuildFromPlayer(sender) == null) {
+      sender.sendMessage(Lang.GUILD_NOT_IN_GUILD.toComponentWithPrefix());
+      return;
+    }
+
+    if (guildModule.getGuildHandler().getGuildFromPlayer(sender) != null) {
+      Guild guild = guildModule.getGuildHandler().getGuildFromPlayer(sender);
+
+      if (!(guild.getOfficers().contains(sender.getUniqueId()) || guild.isLeader(sender.getUniqueId()))) {
+        sender.sendMessage(Lang.GUILD_MUST_BE_OFFICER.toComponentWithPrefix());
+        return;
+      }
+
+      StringBuilder sb = new StringBuilder();
+      for (String arg : args) {
+        sb.append(arg).append(" ");
+      }
+      String name = sb.toString().trim().replace(" ", "");
+
+      Player inv = Bukkit.getPlayer(name);
+
+      if (inv == null) {
+        sender.sendMessage(Lang.PLAYER_NOT_FOUND.toComponentWithPrefix(Map.of("%player%", name)));
+        return;
+      }
+
+      if (guild.getPlayers().contains(Bukkit.getOfflinePlayer(inv.getUniqueId()))) {
+        sender.sendMessage(Lang.GUILD_INVITE_PLAYER_ALREADY_JOINED.toComponentWithPrefix(Map.of("%player%", inv.getName())));
+        return;
+      }
+      if (guild.getInvitedPlayers().contains(inv.getUniqueId())) {
+        sender.sendMessage(Lang.GUILD_INVITE_PLAYER_ALREADY_INVITED.toComponentWithPrefix(Map.of("%player%", inv.getName())));
+
+
+        return;
+      }
+      TextComponent component = Lang.GUILD_INVITED_PLAYER_NOTIFY.toTextComponentWithPrefix(Map.of("%player%", sender.getName(), "%guild%", guild.getName()));
+
+      component = component.toBuilder()
+          .hoverEvent(HoverEvent.showText(Lang.GUILD_INVITED_PLAYER_TOOLTIP.toComponent()))
+          .clickEvent(ClickEvent.runCommand("/guild join " + guild.getName()))
+          .build();
+      inv.sendMessage(component);
+
+      sender.sendMessage(Lang.GUILD_INVITED_PLAYER_ACKNOWLEDGE.toComponentWithPrefix(Map.of("%player%", inv.getName())));
+      guild.sendMessage(Lang.GUILD_INVITED_PLAYER_BROADCAST.toComponentWithPrefix(Map.of("%player%", sender.getName(), "%target%", inv.getName())));
+      guild.getInvitedPlayers().add(inv.getUniqueId());
+    }
   }
 }
