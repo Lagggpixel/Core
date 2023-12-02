11eed287e6f12c0bfd95d926a65fbc279353b01d
Wanbin Cao <93872019+Lagggpixel@users.noreply.github.com>
2023-12-02 15:00:00 +0000

    feat(guild): Added sethome command
    Took 54 minutes

---
+++ src/main/java/me/lagggpixel/core/modules/guilds/commands/subCommands/GuildSetHomeCommand.java
@@ -0,0 +1,53 @@
+package me.lagggpixel.core.modules.guilds.commands.subCommands;
+
+import me.lagggpixel.core.data.Lang;
+import me.lagggpixel.core.modules.guilds.GuildModule;
+import me.lagggpixel.core.modules.guilds.commands.ISubCommand;
+import me.lagggpixel.core.modules.guilds.data.Claim;
+import me.lagggpixel.core.modules.guilds.data.Guild;
+import org.bukkit.Location;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import java.util.Map;
+
+public class GuildSetHomeCommand implements ISubCommand {
+
+  private final GuildModule guildModule;
+
+  public GuildSetHomeCommand(GuildModule guildModule) {
+    this.guildModule = guildModule;
+  }
+
+  @Override
+  public void execute(CommandSender commandSender, String[] args) {
+
+    if (!(commandSender instanceof Player sender)) {
+      commandSender.sendMessage(Lang.PLAYER_ONLY.toComponentWithPrefix());
+      return;
+    }
+
+    Guild guild = guildModule.getGuildHandler().getGuildFromPlayer(sender);
+
+    if (guild == null) {
+      commandSender.sendMessage(Lang.GUILD_NOT_IN_GUILD.toComponentWithPrefix());
+      return;
+    }
+
+    if (guild.isLeader(sender.getUniqueId()) || guild.getOfficers().contains(sender.getUniqueId())) {
+      Location location = sender.getLocation();
+      for (Claim claim : guild.getClaims()) {
+        if (claim.isInside(location, true)) {
+          guild.setHome(location);
+          sender.sendMessage(Lang.GUILD_SETHOME_ACKNOWLEDGE.toComponentWithPrefix(Map.of("%x%", location.getBlockX() + "", "%y%", location.getBlockY() + "", "%z%", location.getBlockZ() + "")));
+          guild.sendMessage(Lang.GUILD_SETHOME_BROADCAST.toComponentWithPrefix(Map.of("%player%", sender.getName(), "%x%", location.getBlockX() + "", "%y%", location.getBlockY() + "", "%z%", location.getBlockZ() + "")));
+          return;
+        }
+      }
+      sender.sendMessage(Lang.GUILD_SETHOME_NOT_IN_CLAIM.toComponentWithPrefix());
+    } else {
+      sender.sendMessage(Lang.GUILD_MUST_BE_OFFICER.toComponentWithPrefix());
+    }
+
+  }
+}
