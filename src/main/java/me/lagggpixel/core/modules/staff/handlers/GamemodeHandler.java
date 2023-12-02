99f890db4dc3ea63a237e6e2ab88dcf4fc406157
Wanbin Cao <93872019+Lagggpixel@users.noreply.github.com>
2023-12-02 13:59:41 +0000

    fix(staff): message fixes
    Took 3 minutes

--- src/main/java/me/lagggpixel/core/modules/staff/handlers/GamemodeHandler.java
+++ src/main/java/me/lagggpixel/core/modules/staff/handlers/GamemodeHandler.java
@@ -12,23 +12,23 @@ public class GamemodeHandler {
   
   public void setGameMode(Player player, GameMode gamemode) {
     player.setGameMode(gamemode);
-    player.sendMessage(Lang.STAFF_GAMEMODE_SELF.toComponentWithPrefix(Map.of("gamemode", gamemode.name().toLowerCase())));
+    player.sendMessage(Lang.STAFF_GAMEMODE_SELF.toComponentWithPrefix(Map.of("%gamemode%", gamemode.name().toLowerCase())));
   }
   
   public void setGameMode(Player sender, Player target, GameMode gamemode) {
     target.setGameMode(gamemode);
-    sender.sendMessage(Lang.STAFF_GAMEMODE_OTHER.toComponentWithPrefix(Map.of("gamemode", gamemode.name().toLowerCase(), "player", target.getName())));
-    target.sendMessage(Lang.STAFF_GAMEMODE_OTHER_NOTIFY.toComponentWithPrefix(Map.of("gamemode", gamemode.name().toLowerCase(), "player", sender.getName())));
+    sender.sendMessage(Lang.STAFF_GAMEMODE_OTHER.toComponentWithPrefix(Map.of("%gamemode%", gamemode.name().toLowerCase(), "%player%", target.getName())));
+    target.sendMessage(Lang.STAFF_GAMEMODE_OTHER_NOTIFY.toComponentWithPrefix(Map.of("%gamemode%", gamemode.name().toLowerCase(), "%player%", sender.getName())));
   }
   
   public void setGameMode(CommandSender sender, Player target, GameMode gamemode) {
     target.setGameMode(gamemode);
-    sender.sendMessage(Lang.STAFF_GAMEMODE_OTHER.toComponentWithPrefix(Map.of("gamemode", gamemode.name().toLowerCase(), "player", target.getName())));
+    sender.sendMessage(Lang.STAFF_GAMEMODE_OTHER.toComponentWithPrefix(Map.of("%gamemode%", gamemode.name().toLowerCase(), "%player%", target.getName())));
     if (sender instanceof ConsoleCommandSender) {
-      target.sendMessage(Lang.STAFF_GAMEMODE_OTHER_NOTIFY.toComponentWithPrefix(Map.of("gamemode", gamemode.name().toLowerCase(), "player", "console")));
+      target.sendMessage(Lang.STAFF_GAMEMODE_OTHER_NOTIFY.toComponentWithPrefix(Map.of("%gamemode%", gamemode.name().toLowerCase(), "%player%", "console")));
     }
     else {
-      target.sendMessage(Lang.STAFF_GAMEMODE_OTHER_NOTIFY.toComponentWithPrefix(Map.of("gamemode", gamemode.name().toLowerCase(), "player", "unknown")));
+      target.sendMessage(Lang.STAFF_GAMEMODE_OTHER_NOTIFY.toComponentWithPrefix(Map.of("%gamemode%", gamemode.name().toLowerCase(), "%player%", "unknown")));
     }
   }
   
