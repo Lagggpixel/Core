40b64bc1d470ee754dbd127815da17200363ecd1
Wanbin Cao <93872019+Lagggpixel@users.noreply.github.com>
2023-12-04 01:18:02 +0000

    feat(guild): added guild command tab completion
    Took 2 minutes

--- src/main/java/me/lagggpixel/core/modules/guilds/commands/GuildCommand.java
+++ src/main/java/me/lagggpixel/core/modules/guilds/commands/GuildCommand.java
@@ -1,6 +1,7 @@
 package me.lagggpixel.core.modules.guilds.commands;
 
 import me.lagggpixel.core.data.CommandClass;
+import me.lagggpixel.core.data.Lang;
 import me.lagggpixel.core.modules.guilds.GuildModule;
 import me.lagggpixel.core.modules.guilds.commands.subCommands.*;
 import me.lagggpixel.core.utils.CommandUtils;
@@ -14,72 +15,75 @@ import java.util.List;
 import java.util.Map;
 
 public class GuildCommand extends CommandClass {
-  
+
   private final GuildModule guildModule;
   private final Map<String, ISubCommand> subCommands;
-  
+
   public GuildCommand(GuildModule guildModule) {
     this.guildModule = guildModule;
     this.subCommands = new HashMap<>();
-    
+
     subCommands.put("create", new GuildCreateCommand(guildModule));
     subCommands.put("invite", new GuildInviteCommand(guildModule));
     subCommands.put("claim", new GuildClaimCommand(guildModule));
     subCommands.put("disband", new GuildDisbandCommand(guildModule));
     subCommands.put("kick", new GuildKickCommand(guildModule));
     subCommands.put("help", new GuildHelpCommand(guildModule));
-    subCommands.put("sethome",  new GuildSetHomeCommand(guildModule));
+    subCommands.put("sethome", new GuildSetHomeCommand(guildModule));
     subCommands.put("join", new GuildJoinCommand(guildModule));
     subCommands.put("who", new GuildWhoCommand(guildModule));
     subCommands.put("map", new GuildMapCommand(guildModule));
   }
 
-  
+
   @Override
   public String getCommandName() {
     return "guild";
   }
-  
+
   @Override
   public String getCommandDescription() {
     return null;
   }
-  
+
   @Override
   public List<String> getCommandAliases() {
     return List.of("guilds", "guild", "g");
   }
-  
+
   @Override
   public String getCommandPermission() {
     return CommandUtils.generateCommandBasePermission(guildModule, this);
   }
-  
+
   @Override
   public String getUsage() {
     return null;
   }
-  
+
   @Override
   public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] args) {
     if (args.length == 0) {
-      // TODO: Handle case where no subcommand is provided
+      commandSender.sendMessage(Lang.INVALID_USAGE.toComponentWithPrefix());
       return true;
     }
-    
+
     String subCommand = args[0].toLowerCase();
-    
+
     if (subCommands.containsKey(subCommand)) {
       subCommands.get(subCommand).execute(commandSender, args);
       return true;
-    } else {
-      // TODO: Handle unknown subcommands
-      return false;
     }
+
+    commandSender.sendMessage(Lang.INVALID_USAGE.toComponentWithPrefix());
+    return true;
   }
-  
+
   @Override
-  public @Nullable List<String> onTabComplete(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] strings) {
+  public @Nullable List<String> onTabComplete(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] args) {
+    if (args.length == 1) {
+      return subCommands.keySet().stream().toList();
+    }
     return null;
   }
 }
