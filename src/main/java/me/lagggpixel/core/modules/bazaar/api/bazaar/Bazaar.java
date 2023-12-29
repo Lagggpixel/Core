2f8636b181cc84cf8d163f488f78d369300bc284
Wanbin Cao <lagggpixel@gmail.com>
2023-12-29 21:02:14 +0800

    fix: updated pom
    Took 38 minutes

--- src/main/java/me/lagggpixel/core/modules/bazaar/api/bazaar/Bazaar.java
+++ src/main/java/me/lagggpixel/core/modules/bazaar/api/bazaar/Bazaar.java
@@ -9,37 +9,37 @@ import java.util.Map;
 import java.util.function.Predicate;
 
 public interface Bazaar {
-    void open(Player player);
+  void open(Player player);
 
-    void open(Player player, Category category);
+  void open(Player player, Category category);
 
-    void openEdit(Player player, Category category);
+  void openEdit(Player player, Category category);
 
-    void openSearch(Player player, String filter);
+  void openSearch(Player player, String filter);
 
-    void openEditSearch(Player player, String filter, ConfigurableMenuItem searchItem);
+  void openEditSearch(Player player, String filter, ConfigurableMenuItem searchItem);
 
-    void openOrders(Player player);
+  void openOrders(Player player);
 
-    void openEditOrders(Player player);
+  void openEditOrders(Player player);
 
-    void openProduct(Player player, Product product);
+  void openProduct(Player player, Product product);
 
-    void openProductEdit(Player player, Product product);
+  void openProductEdit(Player player, Product product);
 
-    List<Category> getCategories();
+  List<Category> getCategories();
 
-    void saveConfig();
+  void saveConfig();
 
-    BazaarAPI getBazaarApi();
+  BazaarAPI getBazaarApi();
 
-    Product getProduct(String id);
+  Product getProduct(String id);
 
-    List<Product> getProducts();
+  List<Product> getProducts();
 
-    List<Product> getProducts(Predicate<Product> filter);
+  List<Product> getProducts(Predicate<Product> filter);
 
-    Map<Product, Integer> getProductsInInventory(Player player);
+  Map<Product, Integer> getProductsInInventory(Player player);
 
-    int getProductAmountInInventory(Product product, Player player);
+  int getProductAmountInInventory(Product product, Player player);
 }
--- src/main/java/me/lagggpixel/core/modules/bazaar/api/menu/ClickActionManager.java
+++ src/main/java/me/lagggpixel/core/modules/bazaar/api/menu/ClickActionManager.java
@@ -8,13 +8,13 @@ import java.util.function.Consumer;
 import java.util.function.Function;
 
 public interface ClickActionManager {
-    void addClickAction(String name, Consumer<ContextClickInfo> action);
+  void addClickAction(String name, Consumer<ContextClickInfo> action);
 
-    void addClickAction(String name, Function<MenuInfo, Consumer<ContextClickInfo>> action);
+  void addClickAction(String name, Function<MenuInfo, Consumer<ContextClickInfo>> action);
 
-    void addEditClickAction(String name, BiFunction<ConfigurableMenuItem, MenuInfo, Consumer<ContextClickInfo>> action);
+  void addEditClickAction(String name, BiFunction<ConfigurableMenuItem, MenuInfo, Consumer<ContextClickInfo>> action);
 
-    Consumer<ContextClickInfo> getClickAction(ConfigurableMenuItem configurableMenuItem, MenuInfo menuInfo, boolean editing);
+  Consumer<ContextClickInfo> getClickAction(ConfigurableMenuItem configurableMenuItem, MenuInfo menuInfo, boolean editing);
 
-    Set<String> getActions();
+  Set<String> getActions();
 }
