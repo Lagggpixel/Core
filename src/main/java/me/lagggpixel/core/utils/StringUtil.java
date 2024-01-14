c5ab4075cdb3197a04eca5ec9d5d9271263ea4bc
Lagggpixel <lagggpixel@gmail.com>
2024-01-14 01:51:56 +0000

    feat: added string utils
    Took 10 seconds

---
+++ src/main/java/me/lagggpixel/core/utils/StringUtil.java
@@ -0,0 +1,67 @@
+package me.lagggpixel.core.utils;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import lombok.experimental.UtilityClass;
+import org.apache.commons.lang.StringUtils;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+@UtilityClass
+public class StringUtil {
+  
+  public interface Case {
+    Case LOWER_CAMEL = new LowerCamelCase();
+    Case SNAKE = new SnakeCase();
+    
+    String toLowerCamel(String s);
+    String toSnake(String s);
+  }
+  
+  @NoArgsConstructor(access = AccessLevel.PRIVATE)
+  public static class LowerCamelCase implements Case {
+    
+    @Override
+    public String toLowerCamel(String s) {
+      return s;
+    }
+    
+    @Override
+    public String toSnake(String s) {
+      final StringBuilder builder = new StringBuilder();
+      
+      for (char c : s.toCharArray()) {
+        if (Character.isUpperCase(c)) builder.append("_");
+        
+        builder.append(Character.toLowerCase(c));
+      }
+      
+      return builder.toString();
+    }
+  }
+  
+  @NoArgsConstructor(access = AccessLevel.PRIVATE)
+  public static class SnakeCase implements Case {
+    
+    @Override
+    public String toLowerCamel(String s) {
+      final String[] split = s.split("_");
+      
+      if (split.length == 1) return s;
+      
+      return split[0] + Arrays.stream(Arrays.copyOfRange(split, 1, split.length))
+          .map(StringUtil::capitalize)
+          .collect(Collectors.joining());
+    }
+    
+    @Override
+    public String toSnake(String s) {
+      return s;
+    }
+  }
+  
+  public String capitalize(String s) {
+    return StringUtils.capitalize(s);
+  }
+}
