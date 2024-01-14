c84a50d33de34c7a9a5138e6547431071b1319c6
Lagggpixel <lagggpixel@gmail.com>
2024-01-14 16:54:45 +0000

    feat(merchant): modified merchant item slot handling
    Took 15 seconds

--- src/main/java/me/lagggpixel/core/modules/merchant/data/MerchantItem.java
+++ src/main/java/me/lagggpixel/core/modules/merchant/data/MerchantItem.java
@@ -7,4 +7,18 @@ import org.bukkit.Material;
 public class MerchantItem {
   private final Material material;
   private final int cost;
+  private final int slot;
+  
+  public int getSlot() {
+    if (slot <= 7) {
+      return slot + 9;
+    } else if (slot <= 14) {
+      return slot + 18;
+    } else if (slot <= 21) {
+      return slot + 27;
+    } else if (slot <= 28) {
+      return slot + 36;
+    }
+    throw new IllegalArgumentException("[Merchant Item] Invalid slot: " + slot);
+  }
 }
