dd1c2c0e179791d57e41f1d0b86cf53e21f5076f
Lagggpixel <lagggpixel@gmail.com>
2024-01-16 12:57:26 +0000

    feat: added tab completion for sub command interface
    Took 23 seconds

--- src/main/java/me/lagggpixel/core/interfaces/ISubCommand.java
+++ src/main/java/me/lagggpixel/core/interfaces/ISubCommand.java
@@ -2,6 +2,10 @@ package me.lagggpixel.core.interfaces;
 
 import org.bukkit.command.CommandSender;
 
+import java.util.List;
+
 public interface ISubCommand {
   void execute(CommandSender commandSender, String[] args);
+  
+  List<String> tabComplete(CommandSender commandSender, String[] args);
 }
