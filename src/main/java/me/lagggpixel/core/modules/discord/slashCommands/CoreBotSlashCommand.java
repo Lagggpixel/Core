cb19fadb7cbd2040ca550f388d04c17810008851
Lagggpixel <lagggpixel@gmail.com>
2024-02-09 20:23:53 +0000

    fix(ticket): fixed admin command not registered correctly
    Took 6 minutes

--- src/main/java/me/lagggpixel/core/modules/discord/slashCommands/CoreBotSlashCommand.java
+++ src/main/java/me/lagggpixel/core/modules/discord/slashCommands/CoreBotSlashCommand.java
@@ -67,7 +67,6 @@ public class CoreBotSlashCommand implements BotSlashCommand {
       options.forEach(slashCommandbuilder::addOption);
     }
     slashCommandbuilder.setEnabledInDms(false);
-    slashCommandbuilder.setDefaultDisabled();
     slashCommandbuilder.setDefaultEnabledForPermissions(PermissionType.ADMINISTRATOR);
 
     slashCommand = slashCommandbuilder.createGlobal(DiscordModule.discordHandler.getDiscordApi()).join();
